2364a55de124f10df7f039c06818f7504f276741
dainmuanynok-cmyk <dainmuanynok@gmail.com>
2025-12-01 23:22:16 +0700

    first commit

---
+++ data/headerInfo.js
@@ -0,0 +1,7 @@
+const Info = {
+  title: "แนะนำทีมฟุตบอล",
+  subtitle: "ทีมฟุตบอล",
+  createBy: "Poom",
+};
+
+export default Info;
---
+++ data/teamFootball.js
@@ -0,0 +1,24 @@
+const teams = [
+    { id: 1, name: "Chelsea"},
+    { id: 2, name: "Manchester City"},
+    { id: 3, name: "Manchester United"},
+    { id: 4, name: "AC Milan"},
+    { id: 5, name: "Inter Milan"},
+    { id: 6, name: "FC Barcelona"},
+    { id: 7, name: "Southampton"},
+    { id: 8, name: "Tottenham Hotspur"},
+    { id: 9, name: "Newcastle United"},
+    { id: 10, name: "Borussia Dortmund"},
+    { id: 11, name: "Bayern Munich"},
+    { id: 12, name: "Napoli"},
+    { id: 13, name: "Arsenal"},
+    { id: 14, name: "Thailand"},
+    { id: 15, name: "England"},
+    { id: 16, name: "Everton"},
+    { id: 17, name: "Leicester City"},
+    { id: 18, name: "Urawa Red Diamonds"},
+    { id: 19, name: "Real Madrid"},
+    { id: 20, name: "Aston Villa"},
+  ];
+
+  export default teams;
